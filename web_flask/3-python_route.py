3fdeb48e28888b2a0f2f26c842e47a7935194f4f
“Dino” <daringamaleldin@hotmail.com>
2024-04-21 01:46:06 -0700

    done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,48 +1,50 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web application.
-"""
+"""Starts a Flask web application.
 
-from flask import Flask, escape
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+"""
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays 'Hello HBNB!'
-    """
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """
-    Route that displays 'HBNB'
-    """
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    Route that displays 'C ' followed by the value of the text variable.
-    Replace underscore _ symbols with a space.
-    """
-    return "C {}".format(escape(text.replace("_", " ")))
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    Route that displays 'Python ' followed by the value of the text variable.
-    Replace underscore _ symbols with a space.
-    The default value of text is 'is cool'.
-    """
-    return "Python {}".format(escape(text.replace("_", " ")))
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
